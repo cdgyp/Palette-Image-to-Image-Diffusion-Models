ac316cd9ec3d2e2fc0b6a9b39f630088bd623825
cdgyp <cdgyp233@qq.com>
2023-02-07 13:19:49 +0800

    仅考虑有东西的区域预测效果

--- models/loss.py
+++ models/loss.py
@@ -11,19 +11,26 @@ from .....common import goal_categories
 #     def forward(self, output, target):
 #         return self.loss_fn(output, target)
 
+def nonempty_points(x: torch.Tensor):
+    mean = x.mean(dim=(-1, -2), keepdim=True)
+    return x >= mean * 0.1
 
 def focused_mse_loss(output: torch.Tensor, target: torch.Tensor):
     len_goal = len(goal_categories)
     weight_goal = torch.full([len_goal], 0.5 / len_goal).to(output.device)
     len_non_goal = target.shape[-3] - len_goal
     weight_non_goal = torch.full([len_non_goal], 0.5 / len_non_goal).to(output.device)
-    weight = torch.cat([weight_goal, weight_non_goal])
-    assert weight.requires_grad == False and (weight.sum() - 1).abs().item() < 1e-5
-    return (weight * ((output - target)**2).mean(dim=(-1, -2))).sum(dim=-1).mean()
+    channel_weight = torch.cat([weight_goal, weight_non_goal])
+    assert channel_weight.requires_grad == False and (channel_weight.sum() - 1).abs().item() < 1e-5
+    nonempty = nonempty_points(target)
+    channelwise_loss = (((output - target)**2) * nonempty).sum(dim=(-1, -2)) / (nonempty.sum(dim=(-1, -2)) + 1e-9)
+    return (channel_weight * channelwise_loss).sum(dim=-1).mean()
 
 def mse_loss(output: torch.Tensor, target: torch.Tensor):
     return F.mse_loss(output, target)
 
+def informative_loss():
+    pass
     
 class FocalLoss(nn.Module):
     def __init__(self, gamma=2, alpha=None, size_average=True):
