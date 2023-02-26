4b34be9bd309e1c026df4b3f6cceb1486ab800ec
cdgyp <cdgyp233@qq.com>
2023-02-27 02:23:45 +0800

    用 dice 处理降噪后期空白区域和有物体的区域数量不平衡的问题

--- models/loss.py
+++ models/loss.py
@@ -28,7 +28,7 @@ def _get_mask():
     assert _mask is not None
     res = _mask
     _mask = None
-    return res
+    return res.clamp(min=0, max=1).round()
 def _get_presence_map():
     global _presence_map
     assert _presence_map is not None
@@ -36,8 +36,24 @@ def _get_presence_map():
     _presence_map = None
     return res
 
+_t_range: 'tuple[int, int]' = None
+_t: int = None
+def set_t_range(t_range: 'tuple[int, int]'):
+    global _t_range
+    _t_range = t_range
+def set_t(t: int):
+    global _t
+    _t = t
+def _get_t():
+    global _t
+    assert _t is not None
+    res = _t
+    _t = None
+    return res
 
-def focused_mse_loss(output: torch.Tensor, target: torch.Tensor):
+
+
+def focused_mse_loss(output: torch.Tensor, target: torch.Tensor, mask: torch.Tensor=None):
     len_goal = len(goal_categories)
     weight_goal = torch.full([len_goal], 0.5 / len_goal).to(output.device)
     len_non_goal = target.shape[-3] - len_goal
@@ -45,9 +61,31 @@ def focused_mse_loss(output: torch.Tensor, target: torch.Tensor):
     channel_weight = torch.cat([weight_goal, weight_non_goal])
     assert channel_weight.requires_grad == False and (channel_weight.sum() - 1).abs().item() < 1e-5
 
-    nonempty_predicted = (_get_presence_map() * _get_mask()).to(channel_weight.device) # 非空且被预测的区域
+    if mask is None: mask = _get_mask()
+    nonempty_predicted = (_get_presence_map() * mask).to(channel_weight.device) # 非空且被预测的区域
     channelwise_loss = (((output - target)**2) * nonempty_predicted).sum(dim=(-1, -2)) / (nonempty_predicted.sum(dim=(-1, -2)) + 1e-9)
-    return (channel_weight * channelwise_loss).sum(dim=-1).mean()
+    return (channel_weight * channelwise_loss).sum(dim=-1)
+
+def _dice_coefficient(output: torch.Tensor, target: torch.Tensor):
+    channel_wise = (output * target).sum(dim=[-1, -2]) / (output ** 2 + target ** 2 + 1e-9).sum(dim=[-1, -2])
+    return 2 * channel_wise.mean(dim=-1)
+
+def _dice_loss(output: torch.Tensor, target: torch.Tensor):
+    return 1 - _dice_coefficient(output, target)
+
+def focused_dice_loss(output: torch.Tensor, target: torch.Tensor, mask: torch.Tensor = None):
+    if mask is None: mask = _get_mask()
+    return _dice_loss(output * mask, target * mask)
+
+def _linear_mix_ratio(t):
+    return (t - _t_range[0]) / (_t_range[1] - _t_range[0] - 1)
+
+def relay_loss(output: torch.Tensor, target: torch.Tensor):
+    t = _get_t()
+    r = _linear_mix_ratio(t)
+    mask = _get_mask()
+    return r * focused_dice_loss(output, target, mask) + (1-r) * focused_mse_loss(output, target, mask)
+
 
 def mse_loss(output: torch.Tensor, target: torch.Tensor):
     return F.mse_loss(output, target)
